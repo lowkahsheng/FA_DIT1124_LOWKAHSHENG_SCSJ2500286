27872e76c93a2e9d35b102c73b3cf90106e69383
LOW KAH SHENG <lowkahsheng2610@gmail.com>
2026-04-06 11:39:44 +0800

    Add files via upload

---
+++ SCSJ2500286_FA_PF/NumberCheckerSectionE.cpp
@@ -0,0 +1,41 @@
+/*
+ * NumberCheckerSectionE.cpp
+ *
+ *  Created on: 6 Apr 2026
+ *      Author: Student
+ */
+
+#include "NumberCheckerSectionE.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+
+	cout << "Enter number:"<< endl;
+
+	for (int number);
+	{
+	    if (number >= 1)
+	    {
+	        number = positive;
+	    }
+	    else
+	    {
+	        number = negative;
+	    }
+	}
+
+    return 0;
+}
+
+
+NumberCheckerSectionE::NumberCheckerSectionE() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NumberCheckerSectionE::~NumberCheckerSectionE() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ SCSJ2500286_FA_PF/NumberCheckerSectionE.h
@@ -0,0 +1,17 @@
+/*
+ * NumberCheckerSectionE.h
+ *
+ *  Created on: 6 Apr 2026
+ *      Author: Student
+ */
+
+#ifndef NUMBERCHECKERSECTIONE_H_
+#define NUMBERCHECKERSECTIONE_H_
+
+class NumberCheckerSectionE {
+public:
+	NumberCheckerSectionE();
+	virtual ~NumberCheckerSectionE();
+};
+
+#endif /* NUMBERCHECKERSECTIONE_H_ */
